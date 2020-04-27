5230c37984f0ebcc81357ffd776277c59d4d0c63
kth496 <taehongkim496@gmail.com>
2020-04-27 15:12:13 +0900

    Modify code indent & header path

--- lab2_bst.c
+++ lab2_bst.c
@@ -1,44 +1,45 @@
 /*
-*	Operating System Lab
-*	    Lab2 (Synchronization)
-*	    Student id : 
-*	    Student name : 
-*
-*   lab2_bst.c :
-*       - thread-safe bst code.
-*       - coarse-grained, fine-grained lock code
-*
-*   Implement thread-safe bst for coarse-grained version and fine-grained version.
-*/
+ *	Operating System Lab
+ *	    Lab2 (Synchronization)
+ *	    Student id :
+ *	    Student name :
+ *
+ *   lab2_bst.c :
+ *       - thread-safe bst code.
+ *       - coarse-grained, fine-grained lock code
+ *
+ *   Implement thread-safe bst for coarse-grained version and fine-grained
+ *version.
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#include "lab2_sync_types.h"
+#include "include/lab2_sync_types.h"
 
 /*
  * TODO
  *  Implement funtction which traverse BST in in-order
- *  
- *  @param lab2_tree *tree  : bst to print in-order. 
+ *
+ *  @param lab2_tree *tree  : bst to print in-order.
  *  @return                 : status (success or fail)
  */
 int lab2_node_print_inorder(lab2_tree *tree) {
-    // You need to implement lab2_node_print_inorder function.
+        // You need to implement lab2_node_print_inorder function.
 }
 
 /*
  * TODO
  *  Implement function which creates struct lab2_tree
  *  ( refer to the ./include/lab2_sync_types.h for structure lab2_tree )
- * 
+ *
  *  @return                 : bst which you created in this function.
  */
 lab2_tree *lab2_tree_create() {
-    // You need to implement lab2_tree_create function.
+        // You need to implement lab2_tree_create function.
 }
 
 /*
@@ -49,95 +50,99 @@ lab2_tree *lab2_tree_create() {
  *  @param int key          : bst node's key to creates
  *  @return                 : bst node which you created in this function.
  */
-lab2_node * lab2_node_create(int key) {
-    // You need to implement lab2_node_create function.
+lab2_node *lab2_node_create(int key) {
+        // You need to implement lab2_node_create function.
 }
 
-/* 
+/*
  * TODO
- *  Implement a function which insert nodes from the BST. 
- *  
+ *  Implement a function which insert nodes from the BST.
+ *
  *  @param lab2_tree *tree      : bst which you need to insert new node.
- *  @param lab2_node *new_node  : bst node which you need to insert. 
+ *  @param lab2_node *new_node  : bst node which you need to insert.
  *  @return                 : satus (success or fail)
  */
-int lab2_node_insert(lab2_tree *tree, lab2_node *new_node){
-    // You need to implement lab2_node_insert function.
-
+int lab2_node_insert(lab2_tree *tree, lab2_node *new_node) {
+        // You need to implement lab2_node_insert function.
 }
 
-/* 
+/*
  * TODO
  *  Implement a function which insert nodes from the BST in fine-garined manner.
  *
- *  @param lab2_tree *tree      : bst which you need to insert new node in fine-grained manner.
- *  @param lab2_node *new_node  : bst node which you need to insert. 
+ *  @param lab2_tree *tree      : bst which you need to insert new node in
+ * fine-grained manner.
+ *  @param lab2_node *new_node  : bst node which you need to insert.
  *  @return                     : status (success or fail)
  */
-int lab2_node_insert_fg(lab2_tree *tree, lab2_node *new_node){
-      // You need to implement lab2_node_insert_fg function.
+int lab2_node_insert_fg(lab2_tree *tree, lab2_node *new_node) {
+        // You need to implement lab2_node_insert_fg function.
 }
 
-/* 
+/*
  * TODO
- *  Implement a function which insert nodes from the BST in coarse-garined manner.
+ *  Implement a function which insert nodes from the BST in coarse-garined
+ * manner.
  *
- *  @param lab2_tree *tree      : bst which you need to insert new node in coarse-grained manner.
- *  @param lab2_node *new_node  : bst node which you need to insert. 
+ *  @param lab2_tree *tree      : bst which you need to insert new node in
+ * coarse-grained manner.
+ *  @param lab2_node *new_node  : bst node which you need to insert.
  *  @return                     : status (success or fail)
  */
-int lab2_node_insert_cg(lab2_tree *tree, lab2_node *new_node){
-    // You need to implement lab2_node_insert_cg function.
+int lab2_node_insert_cg(lab2_tree *tree, lab2_node *new_node) {
+        // You need to implement lab2_node_insert_cg function.
 }
 
-/* 
+/*
  * TODO
  *  Implement a function which remove nodes from the BST.
  *
- *  @param lab2_tree *tree  : bst tha you need to remove node from bst which contains key.
- *  @param int key          : key value that you want to delete. 
+ *  @param lab2_tree *tree  : bst tha you need to remove node from bst which
+ * contains key.
+ *  @param int key          : key value that you want to delete.
  *  @return                 : status (success or fail)
  */
 int lab2_node_remove(lab2_tree *tree, int key) {
-    // You need to implement lab2_node_remove function.
+        // You need to implement lab2_node_remove function.
 }
 
-/* 
+/*
  * TODO
  *  Implement a function which remove nodes from the BST in fine-grained manner.
  *
- *  @param lab2_tree *tree  : bst tha you need to remove node in fine-grained manner from bst which contains key.
- *  @param int key          : key value that you want to delete. 
+ *  @param lab2_tree *tree  : bst tha you need to remove node in fine-grained
+ * manner from bst which contains key.
+ *  @param int key          : key value that you want to delete.
  *  @return                 : status (success or fail)
  */
 int lab2_node_remove_fg(lab2_tree *tree, int key) {
-    // You need to implement lab2_node_remove_fg function.
+        // You need to implement lab2_node_remove_fg function.
 }
 
-
-/* 
+/*
  * TODO
- *  Implement a function which remove nodes from the BST in coarse-grained manner.
+ *  Implement a function which remove nodes from the BST in coarse-grained
+ * manner.
  *
- *  @param lab2_tree *tree  : bst tha you need to remove node in coarse-grained manner from bst which contains key.
- *  @param int key          : key value that you want to delete. 
+ *  @param lab2_tree *tree  : bst tha you need to remove node in coarse-grained
+ * manner from bst which contains key.
+ *  @param int key          : key value that you want to delete.
  *  @return                 : status (success or fail)
  */
 int lab2_node_remove_cg(lab2_tree *tree, int key) {
-    // You need to implement lab2_node_remove_cg function.
+        // You need to implement lab2_node_remove_cg function.
 }
-
 
 /*
  * TODO
  *  Implement function which delete struct lab2_tree
  *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
  *
- *  @param lab2_tree *tree  : bst which you want to delete. 
+ *  @param lab2_tree *tree  : bst which you want to delete.
  *  @return                 : status(success or fail)
  */
 void lab2_tree_delete(lab2_tree *tree) {
-    // You need to implement lab2_tree_delete function.
+        // You need to implement lab2_tree_delete function.
 }
 
 /*
@@ -145,10 +150,9 @@ void lab2_tree_delete(lab2_tree *tree) {
  *  Implement function which delete struct lab2_node
  *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
  *
- *  @param lab2_tree *tree  : bst node which you want to remove. 
+ *  @param lab2_tree *tree  : bst node which you want to remove.
  *  @return                 : status(success or fail)
  */
 void lab2_node_delete(lab2_node *node) {
-    // You need to implement lab2_node_delete function.
+        // You need to implement lab2_node_delete function.
 }
-
