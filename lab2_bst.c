fcc9d58258a7cbc8571dda3261a4ae26020822cc
kth496 <taehongkim496@gmail.com>
2020-05-02 01:55:36 +0900

    Update remove cg

--- lab2_bst.c
+++ lab2_bst.c
@@ -323,7 +323,7 @@ int lab2_node_remove(lab2_tree *tree, int key) {
                 else
                         lab2_node_remove(tmpTree, leastFromRight->key);
         }
-        return 1;
+        return 0;
 }
 
 /*
@@ -437,9 +437,41 @@ int lab2_node_remove_cg(lab2_tree *tree, int key) {
                 }
         }
 
-        /* If there is no key return -1 */
-        if (!curNode)
-                return -1;
+        /* remove root node*/
+        if (!parNode) {
+                if (!curNode->left && !curNode->right) {
+                        tree->root = NULL;
+                }
+
+                else if (!curNode->left != !curNode->right) {
+                        if (curNode->right) {
+                                lab2_node *tmp = curNode->right;
+                                curNode->key = curNode->right->key;
+                                curNode->right = curNode->right->right;
+                                lab2_node_delete(tmp);
+                        } else {
+                                lab2_node *tmp = curNode->left;
+                                curNode->key = curNode->left->key;
+                                curNode->left = curNode->left->left;
+                                lab2_node_delete(tmp);
+                        }
+                }
+
+                else {
+                        lab2_tree *tmpTree = lab2_tree_create();
+                        /* Find least key node */
+                        lab2_node *leastFromRight = curNode->right;
+                        while (leastFromRight->left)
+                                leastFromRight = leastFromRight->left;
+
+                        /* Swap the key and delete node */
+                        curNode->key = leastFromRight->key;
+                        tmpTree->root = leastFromRight;
+                        lab2_node_remove(tmpTree, leastFromRight->key);
+                }
+                pthread_mutex_unlock(&mutex);
+                return 0;
+        }
 
         /*
          * BST Node Remove Rules
@@ -447,6 +479,8 @@ int lab2_node_remove_cg(lab2_tree *tree, int key) {
          * 2. Node that has only one child node.
          * 3. Node that has two child nodes.
          */
+        // printf("%d \n", tree->root == curNode);
+        // printf("%d %d\n", key, curNode->key);
 
         /* CASE 1 */
         if (!curNode->left && !curNode->right) {
@@ -454,18 +488,20 @@ int lab2_node_remove_cg(lab2_tree *tree, int key) {
                         parNode->right = NULL;
                 else
                         parNode->left = NULL;
+                // printf("%d %d %d %d\n", parNode->key, parNode->right->key,
+                //        parNode->left->key, curNode->key);
                 lab2_node_delete(curNode);
         }
 
         /* CASE 2 */
         else if (!curNode->left != !curNode->right) {
                 if (parNode->right->key == curNode->key) {
-                        if (!curNode->right)
+                        if (curNode->right)
                                 parNode->right = curNode->right;
                         else
                                 parNode->right = curNode->left;
                 } else {
-                        if (!curNode->right)
+                        if (curNode->right)
                                 parNode->left = curNode->right;
                         else
                                 parNode->left = curNode->left;
@@ -476,7 +512,7 @@ int lab2_node_remove_cg(lab2_tree *tree, int key) {
         /* CASE 3 */
         // if (curNode->left && curNode->right) {
         else {
-                lab2_tree *tmpTree;
+                lab2_tree *tmpTree = lab2_tree_create();
                 curNode->key = curNode->right->key;
                 /* Find least key node */
                 lab2_node *leastFromRight = curNode->right;
@@ -486,10 +522,13 @@ int lab2_node_remove_cg(lab2_tree *tree, int key) {
                 /* Swap the key and delete node */
                 curNode->key = leastFromRight->key;
                 tmpTree->root = leastFromRight;
-                lab2_node_remove(tmpTree, leastFromRight->key);
+                if (!leastFromRight->left && !leastFromRight->right)
+                        curNode->right = NULL;
+                else
+                        lab2_node_remove(tmpTree, leastFromRight->key);
         }
         pthread_mutex_unlock(&mutex);
-        return 1;
+        return 0;
 }
 
 /*
